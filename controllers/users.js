b7bd0903cf6aac93a954afc7eacf4fdb55c55cd5
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-02 22:25:25 +0300

    fixed a typo (usersArray)

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const sendAllUsers = (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify(req.categoriesArray));
+  res.end(JSON.stringify(req.usersArray));
 };
 
 module.exports = sendAllUsers;
