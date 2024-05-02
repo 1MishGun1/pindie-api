43a0085b3ed15741a9611765a97d18c123ff2029
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-02 22:26:06 +0300

    add saving results

--- middlewares/games.js
+++ middlewares/games.js
@@ -2,6 +2,7 @@ const games = require("../models/game");
 
 const findAllGames = async (req, res, next) => {
   const result = await games.find({}).populate("categories").populate("users");
+  req.gamesArray = result;
   console.log(result);
   next();
 };
