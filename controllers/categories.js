8eb6022e25075d573f30bc96fa7e4b046aaa1acb
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-07 21:12:17 +0300

    add function show result update category

--- controllers/categories.js
+++ controllers/categories.js
@@ -14,5 +14,15 @@ const sendCategoryById = (req, res) => {
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(req.category));
 };
+// Function show result update category
+const sendCategoryUpdated = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).send(JSON.stringify({ message: "Update category" }));
+};
 
-module.exports = { sendAllCategories, sendCategoryCreated, sendCategoryById };
+module.exports = {
+  sendAllCategories,
+  sendCategoryCreated,
+  sendCategoryById,
+  sendCategoryUpdated,
+};
