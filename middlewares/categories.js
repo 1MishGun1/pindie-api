8c911197f99340c9f81cb7dbfbfdd0cc520995d9
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-07 12:43:46 +0300

    add functions: create new category, search category by id

--- middlewares/categories.js
+++ middlewares/categories.js
@@ -1,8 +1,35 @@
 const categories = require("../models/category");
 
 const findAllCategories = async (req, res, next) => {
-  req.categoriesArray = await categories.find({});
+  const result = (req.categoriesArray = await categories.find({}));
+  req.categoriesArray = result;
   next();
 };
 
-module.exports = findAllCategories;
+// Create new category
+const createCategory = async (req, res, next) => {
+  console.log("POST /categories");
+  try {
+    console.log(req.body);
+    req.category = await categories.create(req.body);
+    next();
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res
+      .statusCode(400)
+      .send(JSON.stringify({ message: "Error create new category" }));
+  }
+};
+
+// Search category by id
+const findCategoryById = async (req, res, next) => {
+  try {
+    req.category = await categories.findById(req.params.id);
+    next();
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode(404).send(JSON.stringify({ message: "Category not found" }));
+  }
+};
+
+module.exports = { findAllCategories, createCategory, findCategoryById };
