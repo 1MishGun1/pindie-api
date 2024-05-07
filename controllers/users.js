de25db2657c0557155c1d8a75bf5287feb31ecd8
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-07 12:42:59 +0300

    add functions send a new user, send user by id

--- controllers/users.js
+++ controllers/users.js
@@ -3,4 +3,16 @@ const sendAllUsers = (req, res) => {
   res.end(JSON.stringify(req.usersArray));
 };
 
-module.exports = sendAllUsers;
+// Controller send a new user
+const sendUserCreated = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.user));
+};
+
+// Function send user by id
+const sendUserById = (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(req.user));
+};
+
+module.exports = { sendAllUsers, sendUserCreated, sendUserById };
