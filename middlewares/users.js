672dee2a66b4674f21c225824c29c7d4f462cf2b
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-07 12:44:11 +0300

    add functions: create new user, search user by id

--- middlewares/users.js
+++ middlewares/users.js
@@ -1,8 +1,33 @@
 const users = require("../models/user");
 
 const findAllUsers = async (req, res, next) => {
-  req.usersArray = await users.find({});
+  const result = (req.usersArray = await users.find({}));
+  req.usersArray = result;
   next();
 };
 
-module.exports = findAllUsers;
+// Create new user
+const createUser = async (req, res, next) => {
+  console.log("POST /users");
+  try {
+    console.log(req.body);
+    req.user = await users.create(req.body);
+    next();
+  } catch (error) {
+    res.setHeader("Content-Type", "application/json");
+    res
+      .statusCode(400)
+      .send(JSON.stringify({ message: "Error create new user" }));
+  }
+};
+
+// Search game by id
+const findUserById = async (req, res, next) => {
+  try {
+    req.user = await users.findById(req.params.id);
+  } catch (error) {
+    res.statusCode(404).send(JSON.stringify({ message: "User not found" }));
+  }
+};
+
+module.exports = { findAllUsers, createUser, findUserById };
