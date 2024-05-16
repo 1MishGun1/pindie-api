1d2d59544c1d12d93acb8f4f5e125d6016faa628
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-16 11:06:50 +0300

    add apiRouter

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const usersRouter = require("./routes/users");
-const gamesRouter = require("./routes/games");
-const categoriesRouter = require("./routes/categories");
 
 const connectToDatabase = require("./database/connect");
 const cors = require("./middlewares/cors");
+const apiRouter = require("./routes/apiRouter");
 
 const app = express();
 const PORT = 3000;
@@ -16,10 +14,8 @@ connectToDatabase();
 app.use(
   cors,
   bodyParser.json(),
-  express.static(path.join(__dirname, "public")),
-  usersRouter,
-  gamesRouter,
-  categoriesRouter
+  apiRouter,
+  express.static(path.join(__dirname, "public"))
 );
 
 app.listen(PORT, () => {
