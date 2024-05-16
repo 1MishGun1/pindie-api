68b54318cfa4bf36ec1650fcff0e82560854285f
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-16 21:13:15 +0300

    add next() in hushPassword

--- middlewares/users.js
+++ middlewares/users.js
@@ -90,6 +90,7 @@ const hushPassword = async (req, res, next) => {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(req.body.password, salt);
     req.body.password = hash;
+    next();
   } catch (error) {
     res.status(400).send("Error hash user password");
   }
