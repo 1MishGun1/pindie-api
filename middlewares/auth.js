507ced2c4383afac00eb93ded1478a20ddfc9c89
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-16 21:14:01 +0300

    fix misspell in checkAuth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const checkAuth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  if (!authorization || authorization.startsWith("Bearer ")) {
+  if (!authorization || !authorization.startsWith("Bearer ")) {
     return res.status(401).send("Please log in");
   }
 
