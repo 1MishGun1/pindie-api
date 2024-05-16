d1194df58758bdbff40806e30ab9f0df3bf59418
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-16 12:32:39 +0300

    add function for cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcryptjs");
 const users = require("../models/user");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 
 const login = (req, res) => {
   const { email, password } = req.body;
@@ -25,4 +26,18 @@ const login = (req, res) => {
     });
 };
 
-module.exports = { login };
+// Function for cookies
+const sendIndex = (req, res) => {
+  if (req.cookies.jwt) {
+    try {
+      jwt.verify(req.cookies.jwt, "some-secret-key");
+      res.sendFile(path.join(__dirname, "../public/admin/dashboard.html"));
+    } catch (error) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    }
+  } else {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  }
+};
+
+module.exports = { login, sendIndex };
