93e503456be794d57d89d854f257809e3a88cae5
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-16 14:36:02 +0300

    add checkCookiesJWT function

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const checkAuth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || authorization.startsWith("Bearer ")) {
-    res.status(401).send("Please log in");
+    return res.status(401).send("Please log in");
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -12,9 +12,17 @@ const checkAuth = (req, res, next) => {
   try {
     req.user = jwt.verify(token, "some-secret-key");
   } catch (error) {
-    res.status(401).send("Please log in");
+    return res.status(401).send("Please log in");
   }
   next();
 };
 
-module.exports = { checkAuth };
+const checkCookiesJWT = (req, res, next) => {
+  if (!req.cookies.jwt) {
+    return res.redirect("/");
+  }
+  req.headers.authorization = `Bearer ${req.cookies.jwt}`;
+  next();
+};
+
+module.exports = { checkAuth, checkCookiesJWT };
