42c8d39683770e2a1cff9b73a034c619fe1aeaab
1MishGun1 <mikhail.kudrov@gmail.com>
2024-05-16 11:58:10 +0300

    import checkAuth, add new get method and connection checkAuth in methods

--- routes/games.js
+++ routes/games.js
@@ -11,6 +11,7 @@ const {
   checkIfUsersAreSafe,
   checkIsGameExists,
 } = require("../middlewares/games");
+
 const {
   sendAllGames,
   sendGameCreated,
@@ -19,26 +20,34 @@ const {
   sendGameDeleted,
 } = require("../controllers/games");
 
+const { checkAuth } = require("../middlewares/auth");
+
 gamesRouter.get("/games", findAllGames, sendAllGames);
+
 gamesRouter.post(
   "/games",
   findAllGames,
   checkIsGameExists,
   checkIfCategoriesAvaliable,
   checkEmptyFields,
+  checkAuth,
   createGame,
   sendGameCreated
 );
+
 gamesRouter.get("/games/:id", findGameById, sendGameById);
+
 gamesRouter.put(
   "/games/:id",
   findGameById,
   checkIfUsersAreSafe,
   checkIfCategoriesAvaliable,
   checkEmptyFields,
+  checkAuth,
   updateGame,
   sendGameUpdated
 );
-gamesRouter.delete("/games/:id", deleteGame, sendGameDeleted);
+
+gamesRouter.delete("/games/:id", checkAuth, deleteGame, sendGameDeleted);
 
 module.exports = gamesRouter;
